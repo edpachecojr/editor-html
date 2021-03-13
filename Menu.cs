8fbff13b87e132c75f0007c961b4673736c3a12e
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2021-03-13 10:32:26 -0300

    [feature] desenhando a tela

--- Menu.cs
+++ Menu.cs
@@ -9,6 +9,34 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int columns = 0; columns <= 30; columns++)
+                    Console.Write(" ");
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
         }
     }
 }
